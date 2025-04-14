25e8e98a42e1eb734503577ff2dd26d5d4f00c6b
z8gb193 <deanopatent@hotmail.co.uk>
2025-04-14 06:40:24 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BeeOneAIChat from './BeeOneAIChat';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BeeOneAIChat />
+  </React.StrictMode>
+);
